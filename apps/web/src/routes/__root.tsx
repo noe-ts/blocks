fb2857b8edada8aa29c039387f724200dc40376b
Noé <noe.dubosq.allende@gmail.com>
2026-01-16 08:39:23 +0100

    wip

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -18,7 +18,7 @@ export const Route = createRootRouteWithContext<RouterAppContext>()({
         content: "width=device-width, initial-scale=1",
       },
       {
-        title: "My App",
+        title: "Explorer Map",
       },
     ],
     links: [
--- packages/infra/alchemy.run.ts
+++ packages/infra/alchemy.run.ts
@@ -9,6 +9,7 @@ const app = await alchemy("blocks");
 
 export const web = await TanStackStart("web", {
   cwd: "../../apps/web",
+  domains: ["explore-map.dbsq.fr"],
   bindings: {
     VITE_SERVER_URL: alchemy.env.VITE_SERVER_URL!,
     CORS_ORIGIN: alchemy.env.CORS_ORIGIN!,
